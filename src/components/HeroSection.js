f8856ca69b1ce2a6c2b1069e213829492988dc2c
mbowey <michael.bowey@gmail.com>
2022-02-18 02:01:23 -0500

    connect home page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { useAppContext } from "../AppContext";
+import { injected } from "../connectors";
 import "../App.css";
 import { Button } from "./button";
 import "../styling/HeroSection.css";
+import Text from "../components/Text";
 
-function HeroSection() {
+const pageState = {
+  LOADING: "LOADING",
+  READY: "READY",
+};
+
+const HeroSection = () => {
+  const { activate, active, account } = useWeb3React();
+  const { setContentError } = useAppContext();
+  const [status, setStatus] = useState(pageState.LOADING);
+
+  useEffect(() => {
+    const tryActivate = async () => {
+      await activate(injected, () => {
+        setStatus(pageState.READY);
+      });
+      setStatus(pageState.READY);
+    };
+    tryActivate();
+  }, []);
+
+  if (status === pageState.LOADING) {
+    return <Text>Loading..</Text>;
+  }
+
+  if (status === pageState.READY && !active) {
+    return (
+      <div className="hero-container">
+        <h1>YOUR LUXURY RENTAL AWAITS</h1>
+        <p>What are you waiting for?</p>
+        <div className="hero-btns">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+            onClick={() => {
+              if (!window.ethereum) {
+                setContentError(
+                  "Looks like you don't have Metamask, you'll need it to use this app."
+                );
+                return;
+              }
+              activate(injected, (e) => {
+                if (e instanceof UnsupportedChainIdError) {
+                  setContentError("Network not supported.");
+                }
+              });
+            }}
+          >
+            CONNECT METAMASK WALLET
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  // const NotActive = () => {
+  //   return <Text>Please Connect Your Wallet!!</Text>;
+  // };
+
   return (
     <div className="hero-container">
+      {!active && <NotActive />}
       <h1>YOUR LUXURY RENTAL AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
@@ -28,6 +91,6 @@ function HeroSection() {
       </div>
     </div>
   );
-}
+};
 
 export default HeroSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,8 @@ import HeroSection from "../components/HeroSection";
 import Text from "../components/Text";
 
 const Home = () => {
-  const { active, account } = useWeb3React();
-
-  const NotActive = () => {
-    return <Text>Please Connect Your Wallet!!</Text>;
-  };
-
   return (
     <>
-      {!active && <NotActive />}
       <HeroSection />
     </>
   );
